b159760fc6b336e0f824045288b3fcf5deabe759
MAS364 <siddiquiarshad106@gmail.com>
2023-05-18 22:59:57 +0530

    add

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header({back}) {
+  return (
+    <header className="header">
+      <div className="width">
+        {back && (
+            <Link to="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="48"
+            viewBox="0 0 48 48"
+            width="24"
+          >
+            <path
+              fill="currentColor"
+              d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z"
+            />
+          </svg>
+          </Link>
+        
+        )}
+        <h1>
+          <Link to="/">CoinBit!</Link>
+        </h1>
+      </div>
+    </header>
+  );
+}
